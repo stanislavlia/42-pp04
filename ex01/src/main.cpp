e482de881c126a11906c81f1e6913fd43e1d2df1
stanislavlia <stanislavliashkov@gmail.com>
2024-07-30 19:21:19 +0700

    updated ex01

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -10,6 +10,13 @@
 
 int main()
 {
+	const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+    delete j; //shouldn't create leak
+    delete i;
+
+
     Animal* animals[4];
 
     for (int i = 0; i < 2; i++)
